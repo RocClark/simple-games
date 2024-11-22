756253d7d3882177d8971265b2b533129c457759
Roscoe Clark <roscoe.clark9@gmail.com>
2024-11-22 12:19:00 -0500

    set next app and storybook

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Home() {
+  return (
+    <div>
+           <h1 className="text-4xl font-bold text-center">Hello, world!</h1>
+    </div>
+  );
+}
